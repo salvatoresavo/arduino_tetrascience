a55c0e9be66ecb285bb1dfc08a85d0868d0c7a5e
salvatoresavo <salvatoresavo@users.noreply.github.com>
2014-12-08 15:23:21 -0500

    Update engineTC.py

--- engineTC.py
+++ engineTC.py
@@ -1,40 +1,43 @@
+#!/usr/bin/python
+
+import serial
 import time
 import requests
 import json
-import math
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_MAX31855.MAX31855 as MAX31855
 
-# device information
-device_apikey = 'KTVwgiYj6awRBSEIXvCHm0ePmEuVXea0qTEX80Gp1lhCjLeQ'
-dev_url = "http://localhost:10080/api/log/tcdata"
 device_name = 'sth@gmail.com_Thermocouple'
+interval = 3
+dev_url = "http://192.168.1.10:10080/api/log/tcdata"
 device_type = 'TC'
+device_apikey = 'dsf9usadf['
+
+CLK = 25
+CS = 24
+DO = 18
+
+sensor = MAX31855.MAX31855(CLK,CS,DO)
 
 
-serial_port = '/dev/ttyACM0'
-serial_bauds = 9600
-#device_name = 'thermal couple'
-
-print 'Opening serial port'
-#s = Serial(serial_port,serial_bauds)
-print 'Start reading data from ' + device_name
-#line = s.readline()
 while 1:
-	#line = s.readline()
-	#print line
-	period = 50.0
-	amp = 30.0
-	for count in range(int(-period/2+1), int(period/2)):
-		tmp = round(amp*math.sin(count/period*2*math.pi))
-		print tmp
-		payload = {
-		    'device_type' : device_type,
-		    'device_name' : device_name,
-		    'device_apikey' : device_apikey,
-		    'value' : tmp
-		}
-		r = requests.post(dev_url, data = payload)
-		print(r.text)	        
-		time.sleep(4)
+        tmp = sensor.readTempC()
 
+        print tmp
+        payload = {
+                'device_type' : device_type,
+                'device_name' : device_name,
+                'device_apikey' : device_apikey,
+                'value' : tmp
+        }
 
+        try:
+                r = requests.post(dev_url, data = payload)
+                print(r.text)
+                r = requests.post(dev_url, data = payload)
+                print(r.text)
+        except requests.exceptions.ConnectionError:
+                print 'Connection issue, restart..'
+
+        time.sleep(interval)
 
